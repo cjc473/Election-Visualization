0abb9bb6b634e0c97a0891b844310bcfa584d497
cjc473 <chris.cheasty3@gmail.com>
2021-11-02 22:42:15 -0700

    Add tick to year slider

--- mavericks_and_machins/src/index.js
+++ mavericks_and_machins/src/index.js
@@ -194,6 +194,22 @@ function updateYear(event) {
     2018: 116,
     2020: 117,
   }
+
+  let yearDisplayContainer = document.createElement('p')
+  let yearDisplay = document.createTextNode(`${senateClass[sliderYear]}`);
+  yearDisplayContainer.appendChild(yearDisplay);
+  document.getElementById('year-slider').appendChild(yearDisplayContainer);
+
+  // yearSlider.append('p')
+  //   < span id = "rangeValue" > 0</span>
+  //     <Input class="range" type="range" name "" value = "0" min = "0" max = "1000" onChange = "rangeSlide(this.value)" onmousemove = "rangeSlide(this.value)" ></Input >
+  //   </div >
+  //   <script type="text/javascript">
+  //     function rangeSlide(value) {
+  //       document.getElementById('rangeValue').innerHTML = value;
+  //       }
+
+
   return renderData(CongressApi, senateClass[sliderYear])
 }
 
